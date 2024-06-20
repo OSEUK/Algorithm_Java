89fc588841bcd0bf4b45e0d37edc70a255d6c3c9
OSEUK <tpdnrzmzm3@naver.com>
2024-06-20 14:34:48 +0900

    올바른 괄호

--- src/programmers/kit/stackqueue/Correctss.java
+++ src/programmers/kit/stackqueue/Correctss.java
@@ -3,8 +3,6 @@ package programmers.kit.stackqueue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Stack;
 
 public class Correctss {
